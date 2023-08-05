4a5285784f03e16402741c34193dd60f40de09ce
PatrickLisiecki <impatricklisiecki@gmail.com>
2023-08-04 22:43:22 -0400

    Styled days and added dates for each day

--- client/src/routes/Workouts/WorkoutPlan.jsx
+++ client/src/routes/Workouts/WorkoutPlan.jsx
@@ -5,28 +5,43 @@ import { AuthContext } from "../../contexts/AuthContext";
 const WorkoutPlan = () => {
   const { currentUser } = useContext(AuthContext);
 
+  const getCurrentDate = (separator = "/", offset) => {
+    let newDate = new Date();
+    let date = newDate.getDate() + offset;
+    let month = newDate.getMonth() + 1;
+    let year = newDate.getFullYear();
+
+    return `${month < 10 ? `0${month}` : `${month}`}${separator}${date}${separator}${year}`;
+  };
+
   const daysOfWeek = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
 
   return (
     <div className="w-full h-full flex flex-col items-center">
-      <div className="w-full max-h-[65px] p-4 pl-8 mb-4">
+      <div className="w-full p-4 flex justify-center items-center">
         {/* Username */}
-        <span className="h3 text-[24px] sm:text-[30px] text-center capitalize">
+        <span className="h3 text-[24px] sm:text-[30px] capitalize mb-0">
           {currentUser.username}&apos;s Workout Plan
         </span>
       </div>
 
       {/* Links for days of the week */}
-      <div className="w-full flex flex-col justify-center items-center gap-5">
+      <div className="w-full flex flex-col justify-center items-center gap-6">
         {daysOfWeek.map((day, index) => (
           <div
             key={index}
-            className="max-h-[70px] min-w-[50%] p-6 flex justify-center items-center cursor-pointer border border-primary hover:bg-accent hover:text-white transition-all duration-300"
+            className="w-[50%] min-w-[250px] h-[75px] rounded-lg cursor-pointer bg-white shadow-md hover:shadow-xl transition-all duration-300"
           >
             {/* Use Link to navigate to the corresponding endpoint */}
             {currentUser && (
-              <Link to={`/dashboard/workouts/${day}`} className="w-full text-center">
-                <span className="text-xl uppercase font-bold">{day}</span>
+              <Link
+                to={`/dashboard/workouts/${day}`}
+                className="w-full h-full flex flex-row justify-start items-center"
+              >
+                <div className="w-[50px] h-[50px] flex justify-center items-center mx-4 border border-primary rounded-full text-lg capitalize font-bold">
+                  {day.slice(0, 3)}
+                </div>
+                <div className="text-xl text-secondary">{getCurrentDate("/", index)}</div>
               </Link>
             )}
           </div>
