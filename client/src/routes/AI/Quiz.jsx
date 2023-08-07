6431aeb3c4b9c595bb33c9277ee74eba9afe10a4
Andro Rezkalla <78698964+androrezkalla@users.noreply.github.com>
2023-08-07 12:03:38 -0400

    fixed AI request

--- client/src/routes/AI/Quiz.jsx
+++ client/src/routes/AI/Quiz.jsx
@@ -1,51 +1,44 @@
-import axios from "axios";
-import openai from "openai";
-
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
+import { Configuration, OpenAIApi } from 'openai';
 
 const Quiz = () => {
-  const [generatedWorkout, setGeneratedWorkout] = useState("");
+  const [generatedWorkout, setGeneratedWorkout] = useState('');
 
   const workoutOptions = [
     {
-      field: "workoutType",
-      question: "What type of workout do you prefer?",
-      options: ["Cardio", "Strength", "Yoga", "HIIT"],
+      field: 'workoutType',
+      question: 'What type of workout do you prefer?',
+      options: ['Cardio', 'Strength', 'Yoga', 'HIIT'],
     },
     {
-      field: "intensity",
-      question: "How intense do you want your workout to be?",
-      options: ["Light", "Moderate", "Intense"],
+      field: 'intensity',
+      question: 'How intense do you want your workout to be?',
+      options: ['Light', 'Moderate', 'Intense'],
     },
     {
-      field: "duration",
-      question: "How long do you want your workout to be?",
-      options: ["10 mins", "30 mins", "1 hour", "2 hours"],
+      field: 'duration',
+      question: 'How long do you want your workout to be?',
+      options: ['10 mins', '30 mins', '1 hour', '2 hours'],
     },
     {
-      field: "equipment",
-      question: "Do you have any exercise equipment?",
-      options: ["Yes", "No"],
+      field: 'equipment',
+      question: 'Do you have any exercise equipment?',
+      options: ['Yes', 'No'],
     },
     {
-      field: "fitnessGoal",
-      question: "What are your fitness goals?",
-      options: [
-        "Build Muscle",
-        "Lose Weight",
-        "Improve Flexibility",
-        "Increase Endurance",
-      ],
+      field: 'fitnessGoal',
+      question: 'What are your fitness goals?',
+      options: ['Build Muscle', 'Lose Weight', 'Improve Flexibility', 'Increase Endurance'],
     },
     {
-      field: "fitnessLevel",
-      question: "What is your fitness level?",
-      options: ["Beginner", "Intermediate", "Advanced"],
+      field: 'fitnessLevel',
+      question: 'What is your fitness level?',
+      options: ['Beginner', 'Intermediate', 'Advanced'],
     },
     {
-      field: "days",
-      question: "How many days a week do you plan to workout?",
-      options: ["3 days", "4 days", "5 days", "6 days", "7 days"],
+      field: 'days',
+      question: 'How many days a week do you plan to workout?',
+      options: ['3 days', '4 days', '5 days', '6 days', '7 days'],
     },
   ];
 
@@ -60,10 +53,8 @@ const Quiz = () => {
   });
 
   useEffect(() => {
-    const isAllAnswered = Object.values(answers).every(
-      (answer) => answer !== null
-    );
-    const button = document.getElementById("generate-btn");
+    const isAllAnswered = Object.values(answers).every((answer) => answer !== null);
+    const button = document.getElementById('generate-btn');
     if (button) {
       button.disabled = !isAllAnswered;
     }
@@ -72,44 +63,36 @@ const Quiz = () => {
   const handleAnswerSelection = (questionIndex, optionIndex) => {
     setAnswers({
       ...answers,
-      [workoutOptions[questionIndex].field]:
-        workoutOptions[questionIndex].options[optionIndex],
+      [workoutOptions[questionIndex].field]: workoutOptions[questionIndex].options[optionIndex],
     });
   };
 
   const generateWorkoutPlan = async () => {
-    const prompt = `
-    As a fitness enthusiast, I want a workout plan that fits my preferences.
+    const prompt = `As a fitness enthusiast, I want a workout plan that fits my preferences.
     I prefer ${answers.workoutType} workouts with ${answers.intensity} intensity.
     I want the workout to be ${answers.duration} long and ${answers.equipment} equipment.
     My fitness goal is to ${answers.fitnessGoal}.
     My fitness level is ${answers.fitnessLevel}.
     I plan to work out ${answers.days} a week.
-    Generate a workout plan for me.
-  `;
-    const OPENAI_API_KEY =
-      "Bearer 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";
+    Generate a workout plan for me.`;
+
+    const apiKey = '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'; // Replace with your API key
+    const openAi = new OpenAIApi(
+      new Configuration({
+        apiKey,
+      })
+    );
+
     try {
-      const response = await axios.post(
-        "https://api.openai.com/v1/engines/davinci/completions",
-        {
-          prompt: prompt,
-          max_tokens: 1000,
-        },
-        {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `${OPENAI_API_KEY}`,
-          },
-        }
-      );
-      //console.log("API Response:", response.data);
+      const response = await openAi.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: [{ role: 'system', content: 'You are a fitness enthusiast seeking a workout plan.' }, { role: 'user', content: prompt }],
+      });
 
-      const generatedPlan = response.data.choices[0].text;
-
+      const generatedPlan = response.data.choices[0]?.message?.content;
       setGeneratedWorkout(generatedPlan);
     } catch (error) {
-      console.error("Error generating workout plan:", error);
+      console.error('Error generating workout plan:', error);
     }
   };
 
@@ -129,8 +112,8 @@ const Quiz = () => {
                     }
                     className={`w-full px-4 py-2 rounded-lg ${
                       answers[option.field] === choice
-                        ? "bg-orange-400 text-white"
-                        : "bg-gray-100 text-gray-700 hover:bg-gray-300"
+                        ? 'bg-orange-400 text-white'
+                        : 'bg-gray-100 text-gray-700 hover:bg-gray-300'
                     } transition-colors duration-300 ease-in-out`}
                   >
                     {choice}
@@ -145,17 +128,15 @@ const Quiz = () => {
           onClick={generateWorkoutPlan}
           className={`block mx-auto px-6 py-3 rounded-lg ${
             Object.values(answers).every((answer) => answer !== null)
-              ? "bg-orange-500 text-white hover:bg-orange-600"
-              : "bg-gray-400 text-gray-600 cursor-not-allowed"
+              ? 'bg-orange-500 text-white hover:bg-orange-600'
+              : 'bg-gray-400 text-gray-600 cursor-not-allowed'
           } font-semibold transition-colors duration-300 ease-in-out`}
         >
           Generate Workout Plan
         </button>
         {generatedWorkout && (
           <div className="mt-6">
-            <h3 className="text-xl font-semibold mb-2">
-              Generated Workout Plan
-            </h3>
+            <h3 className="text-xl font-semibold mb-2">Generated Workout Plan</h3>
             <p className="bg-gray-100 p-4 rounded-lg">{generatedWorkout}</p>
           </div>
         )}
@@ -165,5 +146,3 @@ const Quiz = () => {
 };
 
 export default Quiz;
-
-//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
