e4e1bec0d29d0c2e5eeae5b340e1093ec8deffbb
PatrickLisiecki <impatricklisiecki@gmail.com>
2023-08-07 17:50:23 -0400

    Added link to exercise log page

--- client/src/routes/Progress/Progress.jsx
+++ client/src/routes/Progress/Progress.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import axios from "axios";
 import { useState, useEffect, useContext } from "react";
 import { AuthContext } from "../../contexts/AuthContext";
@@ -10,7 +11,12 @@ import { Input, Textarea, Chip, Button } from "@material-tailwind/react";
 
 // Icons
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus, faXmark, faArrowRightLong } from "@fortawesome/free-solid-svg-icons";
+import {
+  faPlus,
+  faXmark,
+  faArrowRightLong,
+} from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 export default function Progress() {
   // Get the current user
@@ -39,45 +45,13 @@ export default function Progress() {
     if (currentUser) {
       const userId = currentUser.id;
 
-      // Fetch exercises data from the server for the specified user, workout, and day
-      axios
-        .get(`/api/exercises/${userId}`)
-        .then((response) => setExercises(response))
-        .catch((error) => console.log(error));
+      // Fetch exercises data from the server for the specified user
+      axios.get(`/api/exercises/${userId}`).then((response) => {
+        const data = response.data;
+        setExercises(data);
+      });
     }
   }, [currentUser]);
-
-  // const handleAddToMyWorkout = (workout) => {
-  //   // Make a POST request to the backend API to add the workout to the user's exercises
-  //   axios
-  //     .post(
-  //       "/api/exercises/",
-  //       {
-  //         userId: currentUser.id,
-  //         day: day,
-  //         name: workout.name,
-  //         type: workout.type,
-  //         muscle: workout.muscle,
-  //         equipment: workout.equipment,
-  //         difficulty: workout.difficulty,
-  //         details: workout.instructions,
-  //         week: week,
-  //         workoutId: workoutId,
-  //       },
-  //       { withCredentials: true },
-  //     )
-
-  //     .then((response) => {
-  //       console.log("Workout added to My Workout:", response.data);
-  //       // After adding a new exercise, update the exercises in the ExerciseContext
-  //       setExercises((prevExercises) => [...prevExercises, response.data]);
-  //       // Call the callback function to inform UserExercisesList that it needs to update
-  //       onExerciseAdd();
-  //     })
-  //     .catch((error) => {
-  //       console.error("Error adding workout:", error);
-  //     });
-  // };
 
   return (
     <>
@@ -85,7 +59,8 @@ export default function Progress() {
         <div className="w-full p-4 text-center">
           <span className="h2">Progress Tracker</span>
         </div>
-        <div className="">
+        {/* Add a new exercise */}
+        {/* <div className="">
           <button
             onClick={showModal}
             className="flex items-center justify-center gap-x-2 rounded bg-accent px-2 py-1 text-white hover:bg-accent/90 sm:px-4 sm:py-3"
@@ -93,56 +68,62 @@ export default function Progress() {
             <FontAwesomeIcon icon={faPlus} />
             <span className="text-lg font-semibold">Log an Exercise</span>
           </button>
-        </div>
-        {exercises.length > 0 &&
-          exercises.map((exercise) => (
-            <div
-              key={exercise.id}
-              className="rounded-lg bg-white p-4 shadow-lg"
-            >
-              {/* Exercise name and delete button */}
-              <div className="flex w-full flex-row items-center justify-between">
-                <span className="text-xl font-bold">{exercise.name}</span>
-                <button className="grid h-[40px] w-[40px] cursor-pointer place-items-center rounded-full text-black transition-all duration-200 hover:bg-red-500 hover:text-white">
-                  <FontAwesomeIcon icon={faXmark} />
-                </button>
-              </div>
+        </div> */}
 
-              {/* Exercise info */}
-              <div className="my-4 flex flex-row flex-wrap items-center gap-x-4 gap-y-2">
-                <Chip
-                  variant="ghost"
-                  size="sm"
-                  color="cyan"
-                  value={exercise.type}
-                />
-                <Chip
-                  variant="ghost"
-                  size="sm"
-                  color="cyan"
-                  value={exercise.muscle}
-                />
-                <Chip
-                  variant="ghost"
-                  size="sm"
-                  color="cyan"
-                  value={exercise.equipment}
-                />
-              </div>
-
-              {/* Button to open exercise modal */}
-              <Button
-                
-                ripple={false}
-                variant="filled"
-                color="cyan"
-                className="flex items-center justify-center gap-x-2"
+        {/* Display user's exercises */}
+        <div className="mt-4 grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+          {exercises.length > 0 &&
+            exercises.map((exercise) => (
+              <Link
+                to={`/dashboard/progress/${exercise.id}`}
+                key={exercise.id}
+                className="rounded-lg bg-white p-4 shadow-lg"
               >
-                <span className="text-white">View Info</span>
-                <FontAwesomeIcon icon={faArrowRightLong} />
-              </Button>
-            </div>
-          ))}
+                {/* Exercise name and delete button
+                <div className="flex w-full flex-row items-center justify-between">
+                  <span className="text-xl font-bold">{exercise.name}</span>
+                  <button className="grid h-[40px] w-[40px] cursor-pointer place-items-center rounded-full text-black transition-all duration-200 hover:bg-red-500 hover:text-white">
+                    <FontAwesomeIcon icon={faXmark} />
+                  </button>
+                </div> */}
+
+                <span className="text-xl font-bold">{exercise.name}</span>
+
+                {/* Exercise info */}
+                <div className="my-4 flex flex-row flex-wrap items-center gap-x-4 gap-y-2">
+                  <Chip
+                    variant="ghost"
+                    size="sm"
+                    color="cyan"
+                    value={exercise.type}
+                  />
+                  <Chip
+                    variant="ghost"
+                    size="sm"
+                    color="cyan"
+                    value={exercise.muscle}
+                  />
+                  <Chip
+                    variant="ghost"
+                    size="sm"
+                    color="cyan"
+                    value={exercise.equipment}
+                  />
+                </div>
+
+                {/* Button to open exercise modal */}
+                <Button
+                  ripple={false}
+                  variant="filled"
+                  color="cyan"
+                  className="flex items-center justify-center gap-x-2"
+                >
+                  <span className="text-white">Log</span>
+                  <FontAwesomeIcon icon={faArrowRightLong} />
+                </Button>
+              </Link>
+            ))}
+        </div>
       </div>
 
       {/* Add an exercise form */}
@@ -171,7 +152,7 @@ export default function Progress() {
             onClick={() => {
               hideModal();
             }}
-            className="flex min-w-[135px] cursor-pointer items-center justify-center gap-x-2 rounded bg-green-500 px-4 py-2 text-white hover:bg-green-500/90"
+            className="flex min-w-[135px] cursor-pointer items-center justify-center gap-x-2 rounded bg-accent px-4 py-2 text-white hover:bg-accent/90"
           >
             <FontAwesomeIcon icon={faPlus} size="sm" />
             <span className="font-semibold">Add Exercise</span>
