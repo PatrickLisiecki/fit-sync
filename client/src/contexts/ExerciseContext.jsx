e4c91ed2bd381098ddb68f024c1f1dd9480e0ce1
PatrickLisiecki <impatricklisiecki@gmail.com>
2023-08-10 21:05:44 -0400

    Disabled prop-types

--- client/src/contexts/ExerciseContext.jsx
+++ client/src/contexts/ExerciseContext.jsx
@@ -1,5 +1,4 @@
 /* eslint-disable react/prop-types */
-// Create a new context file (ExerciseContext.js)
 import { createContext, useState } from "react";
 
 const ExerciseContext = createContext();
