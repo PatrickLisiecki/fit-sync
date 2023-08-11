a6df745ab954939146e3c6e86bf926826b9f5b93
Andro Rezkalla <78698964+androrezkalla@users.noreply.github.com>
2023-08-11 19:13:29 -0400

    delete saved workout button

--- client/src/routes/AI/SavedWorkouts.jsx
+++ client/src/routes/AI/SavedWorkouts.jsx
@@ -1,4 +1,3 @@
-// SavedWorkouts.js
 import { useState, useEffect, useContext } from "react";
 import axios from "axios";
 import { AuthContext } from "../../contexts/AuthContext";
@@ -25,6 +24,15 @@ const SavedWorkouts = () => {
     fetchSavedWorkouts();
   }, [currentUser]);
 
+  const handleDelete = async (workoutId) => {
+    try {
+      await axios.delete(`/api/aiworkouts/${workoutId}`);
+      setSavedWorkouts(savedWorkouts.filter(workout => workout.id !== workoutId));
+    } catch (error) {
+      console.error("Error deleting workout:", error);
+    }
+  };
+
   return (
     <div className="mx-auto mb-8 w-[65%]">
       <div className="w-full p-4 text-center">
@@ -37,6 +45,12 @@ const SavedWorkouts = () => {
             <div className="whitespace-pre-line rounded-lg bg-gray-100 p-4 dark:bg-primary">
               {workout.workout} {/* Render the 'workout' string */}
             </div>
+            <button
+              onClick={() => handleDelete(workout.id)}
+              className="mt-2 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
+            >
+              Delete
+            </button>
           </div>
         ))}
       </div>
