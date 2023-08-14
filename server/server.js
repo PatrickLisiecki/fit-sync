9961343d329fb858afa0ed1c922ed06252ec9718
Andro Rezkalla <78698964+androrezkalla@users.noreply.github.com>
2023-08-14 00:09:25 -0400

    AWS server configuartion + routing

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const session = require("express-session");
 require("dotenv").config();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const path = require("path");
 
 const authRouter = require("./routes/auth");
 const exercisesRouter = require("./routes/exercises");
@@ -59,6 +60,16 @@ app.use("/api/sets", setsRouter);
 app.use("/api/profiles", profilesRouter);
 app.use("/api/aiworkouts", AIworkoutsRouter);
 
+app.use(express.static(path.join(__dirname, "client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/dist", "index.html"));
+});
+
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running at http://localhost:${process.env.PORT}`);
 });
