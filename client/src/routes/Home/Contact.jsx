e83c254f07e5abd592b7402fe3af7a93292325f6
PatrickLisiecki <impatricklisiecki@gmail.com>
2023-08-13 22:57:30 -0400

    Created array for contact cards

--- client/src/routes/Home/Contact.jsx
+++ client/src/routes/Home/Contact.jsx
@@ -3,177 +3,101 @@ import Pat from "/Pat-modified.png";
 import Andro from "/Andro-modified.png";
 import John from "/John-modified.png";
 
+const contactData = [
+  {
+    name: "Patrick Lisiecki",
+    image: Pat,
+    portfolio: "https://patricklisiecki.com/",
+    linkedin: "https://www.linkedin.com/in/patricklisiecki/",
+    github: "https://github.com/PatrickLisiecki",
+  },
+  {
+    name: "Andro Rezkalla",
+    image: Andro,
+    portfolio: "https://www.androrezkalla.com/",
+    linkedin: "https://www.linkedin.com/in/androrezkalla/",
+    github: "https://github.com/androrezkalla",
+  },
+  {
+    name: "John Santiago",
+    image: John,
+    portfolio: "https://johnsantiago.dev/",
+    linkedin: "https://www.linkedin.com/in/john-santiago00/",
+    github: "https://github.com/JohnSantiago00",
+  },
+  {
+    name: "Nicolas Talledo",
+    image: Memodified,
+    portfolio: "https://www.nicktalledo.com/",
+    linkedin: "https://www.linkedin.com/in/nicolas-talledo/",
+    github: "https://github.com/NickTalledo",
+  },
+];
+
 export default function Contact() {
   return (
-    <>
-      <section className="mx-16 lg:mx-32">
-        <h1 className="mb-4 pt-8 text-center text-4xl font-bold">
-          Meet the Founders
-        </h1>
-        <p className="text-center font-semibold">
-          Feel free to reach out! Happy to connect and meet you!
-        </p>
-        <div className="gap-10 lg:flex">
-          <div className="my-10 flex-1 transform rounded-xl p-4  text-center shadow-lg transition-transform hover:scale-105 dark:bg-white">
-            <img src={Pat} width={100} height={100} alt="Patrick" />
-            <h3 className="pb-2 text-lg  font-medium  ">Patrick Lisiecki</h3>
+    <section className="container mx-auto">
+      <h1 className="mb-4 pt-8 text-center text-4xl font-bold">
+        Meet the Founders
+      </h1>
+      <p className="text-center font-semibold">
+        Feel free to reach out! Happy to connect with you!
+      </p>
 
-            <h4 className="py-4 text-accent">Contact</h4>
-            <p className="py-1 text-gray-800">
-              Portfolio:{" "}
-              <a
-                href="https://patricklisiecki.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-orange-500 hover:text-orange-700"
-              >
-                patricklisiecki.com
-              </a>{" "}
-            </p>
-            <p className="py-1 text-gray-800">
-              Social Media:{" "}
-              <a
-                href="https://www.linkedin.com/in/patricklisiecki/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-orange-500 hover:text-orange-700"
-              >
-                LinkedIn.com
-              </a>
-            </p>
-            <p className="py-1 text-gray-800">
-              Projects:{" "}
-              <a
-                href="https://github.com/PatrickLisiecki"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-orange-500 hover:text-orange-700"
-              >
-                GitHub.com
-              </a>{" "}
-            </p>
-          </div>
-          <div className="my-10 flex-1 transform rounded-xl p-4  text-center shadow-lg transition-transform hover:scale-105 dark:bg-white">
-            <img src={Andro} width={100} height={100} alt="Andro" />
-            <h3 className="pb-2 text-lg  font-medium  ">Andro Rezkalla</h3>
+      {/* Contact cards */}
+      <div className="grid grid-cols-1 gap-10 sm:grid-cols-2 ">
+        {contactData.map((contact, index) => {
+          return (
+            <div
+              key={index}
+              className="my-10 flex transform flex-col items-center justify-center rounded-xl p-4 shadow-lg transition-transform hover:scale-105 dark:bg-white"
+            >
+              <img
+                src={contact.image}
+                width={100}
+                height={100}
+                alt={contact.name}
+              />
+              <h3 className="pb-2 text-lg font-semibold">{contact.name}</h3>
 
-            <h4 className="py-4 text-accent">Contact</h4>
-            <p className="py-1 text-gray-800">
-              Portfolio:{" "}
-              <a
-                href="https://www.androrezkalla.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-orange-500 hover:text-orange-700"
-              >
-                androrezkalla.com
-              </a>{" "}
-            </p>
-            <p className="py-1 text-gray-800">
-              Social Media:{" "}
-              <a
-                href="https://www.linkedin.com/in/androrezkalla/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-orange-500 hover:text-orange-700"
-              >
-                LinkedIn.com
-              </a>
-            </p>
-            <p className="py-1 text-gray-800">
-              Projects:{" "}
-              <a
-                href="https://github.com/androrezkalla"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-orange-500 hover:text-orange-700"
-              >
-                GitHub.com
-              </a>{" "}
-            </p>
-          </div>
-        </div>
-        <div className="gap-10 lg:flex">
-          <div className="my-10 flex-1 transform rounded-xl p-4  text-center shadow-lg transition-transform hover:scale-105 dark:bg-white">
-            <img src={John} width={100} height={100} alt="John" />
-            <h3 className="pb-2 text-lg  font-medium  ">John Santiago</h3>
-
-            <h4 className="py-4 text-accent">Contact</h4>
-            <p className="py-1 text-gray-800">
-              Portfolio:{" "}
-              <a
-                href="https://johnsantiago.dev/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-orange-500 hover:text-orange-700"
-              >
-                johnsantiago.com
-              </a>{" "}
-            </p>
-            <p className="py-1 text-gray-800">
-              Social Media:{" "}
-              <a
-                href="https://www.linkedin.com/in/john-santiago00/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-orange-500 hover:text-orange-700"
-              >
-                LinkedIn.com
-              </a>
-            </p>
-            <p className="py-1 text-gray-800">
-              Projects:{" "}
-              <a
-                href="https://github.com/JohnSantiago00"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-orange-500 hover:text-orange-700"
-              >
-                GitHub.com
-              </a>{" "}
-            </p>
-          </div>
-          <div className="my-10 flex-1 transform rounded-xl p-4  text-center shadow-lg transition-transform hover:scale-105 dark:bg-white">
-            <img src={Memodified} width={100} height={100} alt="Nicolas" />
-            <h3 className="pb-2 text-lg  font-medium  ">Nicolas Talledo</h3>
-
-            <h4 className="py-4 text-accent">Contact</h4>
-            <p className="py-1 text-gray-800">
-              Portfolio:{" "}
-              <a
-                href="https://www.nicktalledo.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-orange-500 hover:text-orange-700"
-              >
-                nicktalledo.com
-              </a>{" "}
-            </p>
-            <p className="py-1 text-gray-800">
-              Social Media:{" "}
-              <a
-                href="https://www.linkedin.com/in/nicolas-talledo/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-orange-500 hover:text-orange-700"
-              >
-                LinkedIn.com
-              </a>
-            </p>
-            <p className="py-1 text-gray-800">
-              Projects:{" "}
-              <a
-                href="https://github.com/NickTalledo"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-orange-500 hover:text-orange-700"
-              >
-                GitHub.com
-              </a>{" "}
-            </p>
-          </div>
-        </div>
-      </section>
-    </>
+              <h4 className="py-4 text-accent">Contact</h4>
+              <p className="py-1">
+                Portfolio:{" "}
+                <a
+                  href={contact.portfolio}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-accent hover:text-accent/90"
+                >
+                  {contact.portfolio.substring(8, contact.portfolio.length - 1)}
+                </a>{" "}
+              </p>
+              <p className="py-1">
+                Social Media:{" "}
+                <a
+                  href={contact.linkedin}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-accent hover:text-accent/90"
+                >
+                  LinkedIn
+                </a>
+              </p>
+              <p className="t py-1">
+                Projects:{" "}
+                <a
+                  href={contact.github}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-accent hover:text-accent/90"
+                >
+                  GitHub
+                </a>{" "}
+              </p>
+            </div>
+          );
+        })}
+      </div>
+    </section>
   );
 }
