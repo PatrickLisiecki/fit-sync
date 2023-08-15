a1f4b12ca16f43755b88b09e83667833cb05f7aa
Nick Talledo <nicolastalledo13@gmail.com>
2023-08-15 13:46:59 -0400

    ai workout styling

--- server/client/src/routes/AI/SavedWorkouts.jsx
+++ server/client/src/routes/AI/SavedWorkouts.jsx
@@ -2,9 +2,14 @@ import { useState, useEffect, useContext } from "react";
 import axios from "axios";
 import { AuthContext } from "../../contexts/AuthContext";
 
+// Icons
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown, faXmark } from "@fortawesome/free-solid-svg-icons";
+
 const SavedWorkouts = () => {
   const [savedWorkouts, setSavedWorkouts] = useState([]);
   const { currentUser } = useContext(AuthContext);
+  const [selectedWorkout, setSelectedWorkout] = useState(null); 
 
   useEffect(() => {
     const fetchSavedWorkouts = async () => {
@@ -32,25 +37,43 @@ const SavedWorkouts = () => {
       console.error("Error deleting workout:", error);
     }
   };
+const handleWorkoutClick = (workout) => {
+    setSelectedWorkout(selectedWorkout === workout ? null : workout);
+  };
 
   return (
-    <div className="mx-auto mb-8 w-[65%]">
-      <div className="w-full p-4 text-center">
-        <span className="h2">Saved Workouts</span>
-      </div>
+    <div className="mx-auto mb-8 w-full sm:w-[80%] md:w-[65%]">
       <div className="w-full rounded-lg bg-white px-4 py-4 shadow-md dark:bg-secondary dark:shadow-none">
         {savedWorkouts.map((workout, index) => (
-          <div key={index} className="mb-4">
-            <span className="h3 font-semibold">Workout {index + 1}</span>
-            <div className="whitespace-pre-line rounded-lg bg-gray-100 p-4 dark:bg-primary">
-              {workout.workout} {/* Render the 'workout' string */}
+          <div key={index} className="mb-4 p-4 bg-gray-100 dark:bg-primary rounded-lg">
+            <div className="flex justify-between items-center ">
+              <span
+                className="h3 mb-0 font-semibold cursor-pointer flex items-center"
+                onClick={() => handleWorkoutClick(workout)}
+              >
+                AI Workout {index + 1}
+                <FontAwesomeIcon
+                  icon={faChevronDown}
+                  size="xs"
+                  className={`ml-2 transform transition-all duration-300 ${
+                    selectedWorkout === workout ? "rotate-180" : ""
+                  }`}
+                />
+              </span>
+              <div className="flex flex-row justify-end bg-transparent p-4 items-center">
+                <button
+                  onClick={() => handleDelete(workout.id)}
+                  className="grid h-[50px] w-[50px] place-items-center rounded-full p-3 transition-all duration-300 hover:bg-red-500 dark:text-white"
+                >
+                  <FontAwesomeIcon icon={faXmark} />
+                </button>
+              </div>
             </div>
-            <button
-              onClick={() => handleDelete(workout.id)}
-              className="mt-2 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
-            >
-              Delete
-            </button>
+            {selectedWorkout === workout && (
+              <div className="whitespace-pre-line">
+                {workout.workout}
+              </div>
+            )}
           </div>
         ))}
       </div>
@@ -59,3 +82,4 @@ const SavedWorkouts = () => {
 };
 
 export default SavedWorkouts;
+
--- server/seeders/20230810233451-AIworkout-data.js
+++ server/seeders/20230810233451-AIworkout-data.js
@@ -6,13 +6,117 @@ module.exports = {
     await queryInterface.bulkInsert("ai_workouts", [
       {
         userId: 1,
-        workout: "dcccrcrcrcrcrcrcrc",
+        workout: `
+        **Workout Routine: Full-Body Blast**
+        
+        
+        `,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
         userId: 2,
-        workout: "dcccrcrcrcrcrcrcrc",
+        workout: `
+        **Workout Routine: Full-Body Blast**
+        
+        **Warm-up:**
+        - Jumping Jacks: 2 minutes
+        - Arm Circles: 1 minute
+        - Leg Swings: 1 minute
+        - High Knees: 2 minutes
+        
+        
+        `,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        userId: 1,
+        workout: `
+        **Workout Routine: Full-Body Blast**
+        
+        **Warm-up:**
+        - Jumping Jacks: 2 minutes
+        - Arm Circles: 1 minute
+        - Leg Swings: 1 minute
+        - High Knees: 2 minutes
+        
+        **Circuit: Repeat 3 Times**
+        1. Push-Ups: 12 reps
+        2. Bodyweight Squats: 15 reps
+        3. Bent-Over Rows (using water bottles or light weights): 12 reps each arm
+        4. Lunges: 10 reps each leg
+        5. Plank: Hold for 30 seconds
+        6. Bicycle Crunches: 20 reps each side
+        
+        **Rest:** 1-2 minutes between circuits
+        
+        **Cool Down:**
+        - Seated Forward Fold: Hold for 1 minute
+        - Child's Pose: Hold for 1 minute
+        - Shoulder and Triceps Stretch: 30 seconds each arm
+        - Quad Stretch: 30 seconds each leg
+        `,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        userId: 1,
+        workout: `
+        **Workout Routine: Full-Body Blast**
+        
+        **Warm-up:**
+        - Jumping Jacks: 2 minutes
+        - Arm Circles: 1 minute
+        - Leg Swings: 1 minute
+        - High Knees: 2 minutes
+        
+        **Circuit: Repeat 3 Times**
+        1. Push-Ups: 12 reps
+        2. Bodyweight Squats: 15 reps
+        3. Bent-Over Rows (using water bottles or light weights): 12 reps each arm
+        4. Lunges: 10 reps each leg
+        5. Plank: Hold for 30 seconds
+        6. Bicycle Crunches: 20 reps each side
+        
+        **Rest:** 1-2 minutes between circuits
+        
+        **Cool Down:**
+        - Seated Forward Fold: Hold for 1 minute
+        - Child's Pose: Hold for 1 minute
+        - Shoulder and Triceps Stretch: 30 seconds each arm
+        - Quad Stretch: 30 seconds each leg
+        `,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        userId: 1,
+        workout: `
+        **Workout Routine: Full-Body Blast**
+        
+        **Warm-up:**
+        - Jumping Jacks: 2 minutes
+        - Arm Circles: 1 minute
+        - Leg Swings: 1 minute
+        - High Knees: 2 minutes
+        
+        **Circuit: Repeat 3 Times**
+        1. Push-Ups: 12 reps
+        2. Bodyweight Squats: 15 reps
+        3. Bent-Over Rows (using water bottles or light weights): 12 reps each arm
+        4. Lunges: 10 reps each leg
+        5. Plank: Hold for 30 seconds
+        6. Bicycle Crunches: 20 reps each side
+        
+        **Rest:** 1-2 minutes between circuits
+        
+        **Cool Down:**
+        - Seated Forward Fold: Hold for 1 minute
+        - Child's Pose: Hold for 1 minute
+        - Shoulder and Triceps Stretch: 30 seconds each arm
+        - Quad Stretch: 30 seconds each leg
+        `,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
